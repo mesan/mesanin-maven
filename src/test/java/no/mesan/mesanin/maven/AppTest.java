2b270023a27a2d0724193a6d9fd212ea3614319f
Andreas Pedro Garcia-Vedal <me.colombiano@gmail.com>
2014-08-23 21:56:34 +0200

    Lagt til løsningsforslag for steg-1.

--- src/test/java/no/mesan/mesanin/maven/AppTest.java
+++ src/test/java/no/mesan/mesanin/maven/AppTest.java
@@ -1,33 +1,13 @@
 package no.mesan.mesanin.maven;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest extends TestCase {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest(String testName) {
-        super(testName);
-    }
+import static org.junit.Assert.assertTrue;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite(AppTest.class);
-    }
+public class AppTest {
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp() {
+    @Test
+    public void viktig_sjekk() {
         assertTrue(true);
     }
 }
